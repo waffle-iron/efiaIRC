2e95b887c1185a54cbe1d2d42e85e217192f09b4
Superritchman <superritchman@gmail.com>
2016-12-14 16:54:03 +0100

    Change Type of Id (string to Guid) add static GenerateId-Function (name + random hex value + address to Guid) add static CheckName-Function (min length 4, only a-zA-Z0-9) add static GetRandomHex-Function (to genrate random hexvalue for GenerateId) add InvalidUserNameException

--- classIRC/User.cs
+++ classIRC/User.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Net;
+using System.Text.RegularExpressions;
 
 namespace classIRC
 {
@@ -12,23 +10,55 @@ namespace classIRC
     {
         public string Address { get; private set; }
         public String Name { get; private set; }
-        public String Id { get; private set;  }
+        public Guid Id { get; private set; }
 
-        public User (string address, String name)
+        public User(string address, String name)
         {
             this.Address = address;
-            this.Name = name;
-            this.Id = generateId(address);
+            this.Name = CheckName(name);
+            this.Id = GenerateId(name, address);
         }
 
-        public String getUniqueName ()
+        public String GetUniqueName()
         {
             return Name + "." + Id;
         }
 
-        private String generateId (string addess)
+        private static Guid GenerateId(string name, string address)
         {
-            return null;
+            StringBuilder seed = new StringBuilder(name);
+            seed.Append(address);
+            seed.Append(GetRandomHex(8));
+            Guid guid = new Guid(seed.ToString());
+            return guid;
+        }
+
+        private static string CheckName(string name)
+        {
+            if (Regex.IsMatch(name, @"^[a-zA-Z0-9]{4,}"))
+            {
+                return name;
+            }
+            else
+            {
+                throw new InvalidUserNameException();
+            }
+        }
+
+        private static string GetRandomHex(int digits)
+        {
+            Random random = new Random();
+            byte[] buffer = new byte[digits / 2];
+            string result = String.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
+            if (digits % 2 == 0) return result;
+            return result + random.Next(16).ToString("X");
         }
     }
+}
+
+public class InvalidUserNameException : Exception
+{
+    public InvalidUserNameException() { }
+    public InvalidUserNameException(string message) : base(message) { }
+    public InvalidUserNameException(string message, Exception inner) : base(message, inner) { }
 }
